340487d9cc532be17f0c27142b0bc50cef47d97f
Naser Mirzaei <nasermirzaei89@gmail.com>
2023-02-26 22:47:40 -0500

    Init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/applicaset/buildpack-sample-go
+
+go 1.19
+
+require github.com/nasermirzaei89/env v1.4.0
---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"github.com/nasermirzaei89/env"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	tmpl := template.Must(template.ParseFiles("index.gohtml"))
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_ = tmpl.Execute(w, nil)
+	})
+
+	port := env.GetString("PORT", "8080")
+
+	err := http.ListenAndServe(":"+port, mux)
+	if err != nil {
+		log.Fatalln(fmt.Errorf("error on listen and serve http: %w", err))
+	}
+}
